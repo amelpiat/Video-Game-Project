4c6727b3b1dbe08f11c816e9ba76030c78fbb4be
amelpiat <132037532+amelpiat@users.noreply.github.com>
2024-03-31 02:42:04 -0400

    inventory visuals

---
+++ Assets/Scripts/InventoryBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryBehaviour : MonoBehaviour
+{
+    //use an array to check if an inventory slot is full or not 
+    public bool[] full;
+    public GameObject[] invSlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
