b930a9a10278acefc5d13b0e5aa15d2346658308
Ella Harding <ehardin8@uwo.ca>
2024-04-03 15:42:22 -0400

    Revert "Merge branch 'main' of https://github.com/amelpiat/Project"
    This reverts commit d31e0007a5dd5e979d470c460de6104c78f33bc7, reversing
    changes made to 1f8a0b68c91111dbd8aac8d058394f98e4b25ab9.

--- Assets/Scripts/pinkPotionScript.cs
+++ Assets/Scripts/pinkPotionScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class pinkPotionScript : MonoBehaviour
 {
+<<<<<<< HEAD
     public GameObject Princess;
     public Image healthBar; 
 
+=======
+    public GameObject PrincessVarient;
+>>>>>>> 3b72bb0e6cc993f58eaadc5e07e3707aa63a3381
     // Start is called before the first frame update
     void Start()
     {
@@ -23,15 +26,8 @@ public class pinkPotionScript : MonoBehaviour
     {
         if (other.name == "Princess")
         {
-            if (healthBar.fillAmount == 1f)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                healthBar.fillAmount += 0.1f;
-                Destroy(gameObject);
-            }
+            //Add 1 to points.
+            Destroy(gameObject); //This Destroys
         }
     }
 }
