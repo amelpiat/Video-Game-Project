e43b4131340434a949d7a55dbc816bd15f588a4c
stheri0330 <stheri@uwo.ca>
2024-04-03 21:52:03 -0400

    door

---
+++ Assets/Scripts/OnKeyOpenDoor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKeyOpenDoor : MonoBehaviour
+{
+    public GameObject Instruction;
+    public GameObject AnimeObject;
+    public bool Action = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instruction.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "princess")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+                Instruction.SetActive(false);
+                AnimeObject.GetComponent<Animator>().Play("DoorOpen");
+                Action = false;
+            }
+        }
+    }
+
+}
