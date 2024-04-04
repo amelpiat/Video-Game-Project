1566246d52437b9423dbd2358e13d34f3f6e661a
stheri0330 <stheri@uwo.ca>
2024-04-04 19:10:58 -0400

    door

---
+++ Assets/keyScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyScript : MonoBehaviour
+{
+    public GameObject doorCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorCollider.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "princess")
+        {
+            doorCollider.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
