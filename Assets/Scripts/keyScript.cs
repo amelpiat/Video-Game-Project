d8a77c945b9d65a2f98dd5fcf5b223fa5169c2fb
stheri0330 <stheri@uwo.ca>
2024-04-04 23:51:12 -0400

    some sounds

--- Assets/Scripts/keyScript.cs
+++ Assets/Scripts/keyScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class keyScript : MonoBehaviour
 {
     public GameObject doorCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
