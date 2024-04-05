a2e629e174de6eeb02b5373424b249a2d2f718f5
Ella Harding <ehardin8@uwo.ca>
2024-04-05 00:03:53 -0400

    test

--- Assets/keyScript.cs
+++ Assets/keyScript.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class keyScript : MonoBehaviour
 {
     public GameObject doorCollider;
-    
     // Start is called before the first frame update
     void Start()
     {
-        doorCollider.SetActive(false);
+        doorCollider.SetActive(true);
     }
 
     void OnTriggerEnter(Collider other)
